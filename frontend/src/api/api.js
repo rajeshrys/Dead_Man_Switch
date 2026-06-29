b86393c5eec804ffcb8fea307cd3c92f7d4cbc5d
That Rajesh <rajeshthati535@gmail.com>
2026-06-29 21:24:12 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://dead-man-switch-vs7s.onrender.com/api/v1',
     withCredentials:true
 })
 
